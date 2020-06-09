acb2399f195315386b3df72b325edf5a89724933
harysuryanto <hary.suryanto01@gmail.com>
2020-06-09 23:11:00 +0700

    updated base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = ('https://kabar-langit.herokuapp.com/') ?? 'http://localhost/tubes-tekweb/';
+$config['base_url'] = 'https://kabar-langit.herokuapp.com/' || 'http://localhost/tubes-tekweb/';
 
 /*
 |--------------------------------------------------------------------------
