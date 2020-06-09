2e411df1282dcfe6bb29f8d1ed3e47b570f08afa
harysuryanto <hary.suryanto01@gmail.com>
2020-06-09 23:02:28 +0700

    updated base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/tubes-tekweb/';
+$config['base_url'] = (getenv('REMOTE_ADDR')) ?? 'http://localhost/tubes-tekweb/';
 
 /*
 |--------------------------------------------------------------------------
